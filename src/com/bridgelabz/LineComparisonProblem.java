59e89c3184c4ab387e566134213d80fc7731a026
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-29 20:19:10 +0530

    Welcome to line comparison computation program

---
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+    }
+}
