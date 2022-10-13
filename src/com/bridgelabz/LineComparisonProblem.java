06eb0dddf4df23a8f53ce6228efa5d6a29fbdaad
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-10-13 23:10:39 +0530

    Added: Used equals and compareTo methods of Oops

--- src/com/bridgelabz/LineComparisonProblem.java
+++ src/com/bridgelabz/LineComparisonProblem.java
@@ -1,53 +1,30 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class LineComparisonProblem {
-    static int calculateLength(int x1, int y1, int x2, int y2){
+    int calculateLength(int x1, int y1, int x2, int y2){
         int length=(int)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
         return length;
     }
-    static void equals(int length1,int length2){
-        if(length1==length2){
-            System.out.println("Both lines are equal");
-        }
-        else{
-            System.out.println("Both lines are not equal");
-        }
-    }
-    static void compareTo(int length1,int length2){
-        if(length1==length2){
-            System.out.println("Line1 is equal to Line2");
-        }
-        else if(length1>length2){
-            System.out.println("Line1 is greater then Line2");
-        }
-        else{
-            System.out.println("Line1 is less then Line2");
-        }
-    }
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation program");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the X coordinates of the line1 ");
-        int x1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        System.out.print("Enter the Y coordinates  of the line1 ");
-        int y1 = sc.nextInt();
-        int y2 = sc.nextInt();
-        System.out.print("Enter the X coordinates of the line2 ");
-        int x3 = sc.nextInt();
-        int x4 = sc.nextInt();
-        System.out.print("Enter the Y coordinates  of the line2 ");
-        int y3 = sc.nextInt();
-        int y4 = sc.nextInt();
-        System.out.println("Coordinates of the line1 are => ("+x1+","+y1+") and ("+x2+","+y2+")");
-        System.out.println("Coordinates of the line2 are => ("+x3+","+y3+") and ("+x4+","+y4+")");
-        int result1=calculateLength(x1,y1,x2,y2);
-        int result2=calculateLength(x3,y3,x4,y4);
-        System.out.println("Length of line1 is " + result1);
-        System.out.println("Length of line2 is " + result2);
-        equals(result1,result2);
-        compareTo(result1,result2);
+        LineComparisonProblem length=new LineComparisonProblem();
+        Integer lengthOfLine1=length.calculateLength(8,8,4,5);
+        Integer lengthOfLine2=length.calculateLength(18,14,10,8);
+        System.out.println("Length of first line is ->"+lengthOfLine1);
+        System.out.println("Length of Second line is ->"+lengthOfLine2);
+        if(lengthOfLine1.equals(lengthOfLine2)==true){
+            System.out.println("Lines are equal");
+        }
+        else {
+            System.out.println("Lines are not equal");
+        }
+        if(lengthOfLine1.compareTo(lengthOfLine2)==0){
+            System.out.println("Both the lines are equal");
+        }
+        else if (lengthOfLine1.compareTo(lengthOfLine2)==1) {
+            System.out.println("First line is larger then second line");
+        }
+        else {
+            System.out.println("First line is smaller then the second line");
+        }
     }
 }
